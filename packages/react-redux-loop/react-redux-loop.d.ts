d7c0d356ddf04088c0527c8c4dd865ca9ff7a70f
Anton Korzunov <thekashey@gmail.com>
2018-03-28 16:07:35 +1100

    fix d.st

--- packages/react-redux-loop/react-redux-loop.d.ts
+++ packages/react-redux-loop/react-redux-loop.d.ts
@@ -2,8 +2,41 @@ declare module 'react-redux-loop' {
 
     import * as React from 'react';
 
-    export class ReduxLoop extends React.Component<{
-        children: (event: any) => void;
-    }> {
+    interface Event {
+        type: string,
+        payload: any
     }
+
+    type RenderFn = (event: Event) => void;
+
+    interface LoopProps {
+        children: RenderFn
+    }
+
+    interface TriggerProps {
+        when: string,
+        then: RenderFn
+    }
+
+    /**
+     * Decalarative way to monitor all redux actions
+     */
+    export class ReduxLoop extends React.Component<LoopProps> {}
+
+    /**
+     * Declarative way to react to redux actions
+     */
+    export class ReduxTrigger extends React.Component<TriggerProps> {}
+
+    /**
+     * Redux-loop middleware
+     */
+    export const loopMiddleware: (store: any) => any;
+
+    /**
+     * HOC to make Redux-reactive component
+     * @param {"react-redux-loop".RenderFn} callback
+     * @return {React.ComponentType<any>}
+     */
+    export default function reduxLoop(callback: RenderFn): React.ComponentType<any>;
 }
