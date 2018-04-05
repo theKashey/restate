6f95eff0f64b56511543eca1e5789db0c22794e8
Anton Korzunov <thekashey@gmail.com>
2018-04-05 12:25:55 +1000

    fix d.ts

--- packages/react-redux-restate/react-redux-restate.d.ts
+++ packages/react-redux-restate/react-redux-restate.d.ts
@@ -1,47 +1,50 @@
 declare module 'react-redux-restate' {
-  import * as React from 'react';
+    import * as React from 'react';
 
-  interface RestateOptions {
-      /**
-       * will defer any incoming change
-       */
-      async?: boolean;
-      /**
-       * the key for a `default` store.  Default `store`.
-       */
-      storeKey?: boolean;
-      /**
-       * the key for a result store. Default `store`.
-       */
-      restateKey?: boolean;
-      /**
-       * State comparison function
-       * @param oldStore
-       * @param newStore
-       * @return {boolean}
-       */
-      areStatesEqual?: (oldStore: any, newStore: any) => boolean;
-  }
+    interface RestateOptions {
+        /**
+         * will defer any incoming change
+         */
+        async?: boolean;
+        /**
+         * the key for a `default` store.  Default `store`.
+         */
+        storeKey?: boolean;
+        /**
+         * the key for a result store. Default `store`.
+         */
+        restateKey?: boolean;
+        /**
+         * State comparison function
+         * @param oldStore
+         * @param newStore
+         * @return {boolean}
+         */
+        areStatesEqual?: (oldStore: any, newStore: any) => boolean;
+    }
 
-  /**
-   * Creates a Reprovider Component, which exports `oldStoreName` redux store, as a `newStoreName`.
-   * @param {string} newStoreName
-   * @param {string} oldStoreName = 'store'
-   * @return {React.ComponentClass<any>}
-   */
-  export function reprovide(newStoreName: string, oldStoreName?: string): React.ComponentClass<any>;
-  /**
-   * Redux-restate HOC
-   * @param {ObjectOf(store | string)} extraStores
-   * @param {ObjectOf(state), props: any) => any} composeState
-   * @param {ObjectOf(dispatch), event: any, props: any) => void} routeDispatch
-   * @param {{async?: boolean; areStatesEqual?: ((oldStore: any, newStore: any) => boolean)}} options
-   * @return {(WrappedComponent: (React.ComponentClass<any> | React.StatelessComponent<any>)) => React.ComponentClass<any>}
-   */
-  export default function restate<T>(
-    extraStores: { [K in keyof T]: any },
-    composeState: (states: { [K in keyof T]: any }, props: any) => any,
-    routeDispatch: (dispatches: { [K in keyof T]: any }, event: any, props: any) => void,
-    options: (props) => RestateOptions
-  ): (WrappedComponent: React.ComponentClass<any> | React.StatelessComponent<any>) => React.ComponentClass<any>;
+    type ExtendedT<T> = {default: any} & T;
+    type EventCallback = (event: {type:string}) => void;
+
+    /**
+     * Creates a Reprovider Component, which exports `oldStoreName` redux store, as a `newStoreName`.
+     * @param {string} newStoreName
+     * @param {string} oldStoreName = 'store'
+     * @return {React.ComponentClass<any>}
+     */
+    export function reprovide(newStoreName: string, oldStoreName?: string): React.ComponentClass<any>;
+    /**
+     * Redux-restate HOC
+     * @param {ObjectOf(store | string)} extraStores
+     * @param {ObjectOf(state), props: any) => any} composeState
+     * @param {ObjectOf(dispatch), event: any, props: any) => void} routeDispatch
+     * @param {{async?: boolean; areStatesEqual?: ((oldStore: any, newStore: any) => boolean)}} options
+     * @return {(WrappedComponent: (React.ComponentClass<any> | React.StatelessComponent<any>)) => React.ComponentClass<any>}
+     */
+    export default function restate<T, EXT = ExtendedT<T>>(
+        extraStores: T,
+        composeState: (states: EXT, props: any) => any,
+        routeDispatch: (dispatches: { [K in keyof ExtendedT<T>]: EventCallback }, event: any, props: any) => void,
+        options?: (props) => RestateOptions
+    ): (WrappedComponent: React.ComponentClass<any> | React.StatelessComponent<any>) => React.ComponentClass<any>;
 }
